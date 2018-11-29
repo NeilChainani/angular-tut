99bdd188b9f40465ae4d7491c9f8fce7cfdb99d7
Neil Chainani <Neil.Chainani@lexisnexisrisk.com>
2018-11-29 15:35:15 -0500

    pipes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,11 +3,16 @@ import { CoursesService } from "./courses.service";
 @Component({
   selector: "courses",
   template: `
-    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+    {{ course.title | uppercase | lowercase }} <br />
+    {{ course.students | number }} <br />
+    {{ course.rating | number: "2.2-2" }} <br />
+    {{ course.price | currency: "AUD" }} <br />
+    {{ course.releaseDate | date: "shortDate" }}
   `
 })
 export class CoursesComponent {
   title = "test me";
+  rating: 5;
   colSpan = 2;
   email = "neilchainani1@gmail.com";
 
@@ -20,6 +25,13 @@ export class CoursesComponent {
   onKeyUp() {
     console.log(this.email);
   }
+  course = {
+    title: "neil",
+    rating: 5.423,
+    students: 3121,
+    price: 190.31,
+    releaseDate: new Date(2016, 3, 1)
+  };
   courses;
   color = "red";
   constructor(service: CoursesService) {
